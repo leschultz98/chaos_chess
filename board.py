89191926ec684ed1fa624d14facdc6b52fbcd9ae
NGUYỄN MINH TRƯỜNG <1612760@student.hcmus.edu.vn>
2020-05-11 19:23:03 +0700

    can cuc

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 from cellStatus import CellStatus
 from cell import Cell
-
+from gameStatus import GameStatus
 
 class Board:
     def __init__(self, height, width):
@@ -24,4 +24,74 @@ class Board:
 
     def setCellStatus(self, cell):
         self.map[cell.posX][cell.posY] = cell.status
+    
+    def checkMoveAvailable(self, cell):
+        height=self.height
+        width=self.width
+        map=self.map
+        x = cell.posX
+        y = cell.posY
+        status = cell.status
+        if x not in range(height) and y not in range(width):
+            return False
+        if map[x][y] != CellStatus.NONE or self.__canFindWay(status, [], [cell]) == False:
+            return False
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i in range(height) and j in range(width) and map[i][j] == status:
+                    return True
+        return False
 
+    def canFindWay(self, status, listCellHadCheck, queueWillCheckCell):
+        # {posX, posY, status} = cell
+        # {map, width, height} = board
+        map = self.map
+        width = self.width
+        height = self.height
+
+        cell = queueWillCheckCell.pop(0)
+        listCellHadCheck.append([cell.posX, cell.posY])
+        x = cell.posX
+        y = cell.posY
+
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i in range(height) and j in range(width):
+                    if map[i][j] == CellStatus.NONE and [i, j] not in listCellHadCheck:
+                        queueWillCheckCell.append(Cell(map[i][j], i, j))
+                    elif map[i][j] != status and (map[i][j] == CellStatus.X or map[i][j] == CellStatus.O):
+                        return True
+
+        if len(queueWillCheckCell) > 0:
+            return self.canFindWay(status, listCellHadCheck, queueWillCheckCell)
+        return False
+
+    def checkAttack(self, cell):
+        map = self.map
+        width = self.width
+        height = self.height
+        x = cell.posX
+        y = cell.posY
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i in range(0, height) and j in range(0, width) and map[i][j] != map[x][y] and map[i][j] != CellStatus.NONE:
+                    map[i][j] = CellStatus.BLOCK
+
+    def checkWin(self):
+        maps = board.map
+        maps_flat = mapFlat(maps)
+        if CellStatus.X not in maps_flat:
+            return GameStatus.O_WIN
+        if CellStatus.O not in maps_flat:
+            return GameStatus.X_WIN
+        # check
+        for index, cell in enumerate(maps_flat):
+            if cell == CellStatus.NONE:
+                x = index // self.board.width
+                y = index % self.board.width
+                if self.__canFindWay(CellStatus.O, [], [Cell(CellStatus.NONE, x, y)]):
+                    if self.__canFindWay(CellStatus.X, [], [Cell(CellStatus.NONE, x, y)]):
+                        return GameStatus.UNKNOW
+
+        return self.__statistics()
+def mapFlat(maps): return np.array(maps).flatten().tolist()
--- game.py
+++ game.py
@@ -13,78 +13,13 @@ class Game:
     def __init__(self):
         # height = int(input("nhap chieu cao (height): "))
         # width = int(input("nhap chieu rong (width): "))
-        height, width = 3,3
+        height, width = 3, 3
 
         self.board = Board(height, width)
         self.isTurnX = True
         self.winner = None
 
-    def __checkMoveAvailable(self,board, cell):
-        
-        x = cell.posX
-        y = cell.posY
-        status = cell.status
-        if x not in range(board.height) and y not in range(board.width):
-            return False
-        if board.map[x][y] != CellStatus.NONE or self.__canFindWay(status, [], [cell]) == False:
-            return False
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
-                if i in range(board.height) and j in range(board.width) and board.map[i][j] == status:
-                    return True
-        return False
-
-    def __checkAttack(self, cell):
-        board = self.board
-        maps = board.map
-        x = cell.posX
-        y = cell.posY
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
-                if i in range(0, board.height) and j in range(0, board.width) and maps[i][j] != maps[x][y] and maps[i][j] != CellStatus.NONE:
-                    maps[i][j] = CellStatus.BLOCK
-
-    def __checkWin(self, board):
-        maps = board.map
-        maps_flat = mapFlat(maps)
-        if CellStatus.X not in maps_flat:
-            return GameStatus.O_WIN
-        if CellStatus.O not in maps_flat:
-            return GameStatus.X_WIN
-        # check
-        for index, cell in enumerate(maps_flat):
-            if cell == CellStatus.NONE:
-                x = index // self.board.width
-                y = index % self.board.width
-                if self.__canFindWay(CellStatus.O, [], [Cell(CellStatus.NONE, x, y)]):
-                    if self.__canFindWay(CellStatus.X, [], [Cell(CellStatus.NONE, x, y)]):
-                        return GameStatus.UNKNOW
-
-        return self.__statistics()
-
-    def __canFindWay(self, status, listCellHadCheck, queueWillCheckCell):
-        # {posX, posY, status} = cell
-        # {map, width, height} = board
-
-        cell = queueWillCheckCell.pop(0)
-        listCellHadCheck.append([cell.posX, cell.posY])
-        x = cell.posX
-        y = cell.posY
-        map = self.board.map
-        width = self.board.width
-        height = self.board.height
-
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
-                if i in range(height) and j in range(width):
-                    if map[i][j] == CellStatus.NONE and [i, j] not in listCellHadCheck:
-                        queueWillCheckCell.append(Cell(map[i][j], i, j))
-                    elif map[i][j] != status and (map[i][j] == CellStatus.X or map[i][j] == CellStatus.O):
-                        return True
-
-        if len(queueWillCheckCell) > 0:
-            return self.__canFindWay(status, listCellHadCheck, queueWillCheckCell)
-        return False
+    
 
     def __statistics(self):
         maps = self.board.map
@@ -111,9 +46,9 @@ class Game:
                              ' (Enter "q" to end game): ')
 
             pos_x, pos_y = [int(x) for x in position.split()]
-            
+
             cell = Cell(status, pos_x, pos_y)
-            if self.__checkMoveAvailable(self.board,cell):
+            if self.__checkMoveAvailable(self.board, cell):
                 self.board.setCellStatus(cell)
                 if self.__checkAttack(cell):
                     pass  # todo
@@ -137,8 +72,7 @@ class Game:
 
     def miniMax(self, board, isTurnX):
         board.printBoard()
-        
-        
+
         status = CellStatus.X if isTurnX else CellStatus.O
         state = self.__checkWin(board)
         stateValue = {GameStatus.X_WIN: 10,
@@ -169,7 +103,7 @@ class Game:
         for index, _ in enumerate(maps_flat):
             posX = index // board.width
             posY = index % board.width
-            if self.__checkMoveAvailable(board,Cell(status, posX, posY)):
+            if self.__checkMoveAvailable(board, Cell(status, posX, posY)):
                 result.append([posX, posY])
         return result
 
@@ -184,7 +118,7 @@ class Game:
         pass
 
 
-def mapFlat(maps): return np.array(maps).flatten().tolist()
+
 
 
 Game().play()
